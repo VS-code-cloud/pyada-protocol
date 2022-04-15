e0cf0010338ae6f277707879f60469ad14be6c98
Vishnu Sharma <vishnu.sharma7193240@gmail.com>
2022-04-15 23:54:18 +0000

    bug fixes

--- static/js/moralis.js
+++ static/js/moralis.js
@@ -114,11 +114,11 @@ async function getNFTs(address) {
 }
 
 async function transferMaticToUser(value) {
+  const privateKey = process.env['privateKey']
   let userWallet = await getAddress();
   let web3 = await Moralis.enableWeb3({
         chainId: 0x13881,
-        privateKey:
-          "3ec6ae487b65a47fe0633edca65a393ea05369b85f7cb58f6d24044172708f66"
+        privateKey: privateKey
   });
 
 
@@ -129,56 +129,62 @@ async function transferMaticToUser(value) {
     receiver: userWallet
   };
   let transaction = await Moralis.transfer(options);
+  let result = await transaction.wait();
+  return result
 }
 
-// Pawning NFTs
+
 async function pawnNFT(address, loanAmount, loanRate, loanDuration, tokenid, imageURI, symbol, name, description) {
   
   var modal = document.getElementById("pawnModal");
   var confirmBtn = document.getElementById("confirmPawn");
   var span = document.getElementById("close-pawn-modal");
+  var loadModal = document.getElementById("loadModal");
   var nftWorth = document.getElementById("NFT-worth");
-  /* TODO ONCE PRICE PREDICTOR IS COMPLETE: ADD PRICE PREDICTOR
-  TO TEXT INSTEAD OF "NOT PREDICTED YET"*/
-  nftWorth.innerHTML = nftWorth.innerHTML + " a value not predicted yet"
+  // TODO ONCE PRICE PREDICTOR IS COMPLETE: ADD PRICE PREDICTOR
+  //TO TEXT INSTEAD OF "NOT PREDICTED YET"
+  let resp = await fetch(`https://pyadaprotocol.randomcodingboy.repl.co/api/getQuotePrice?address=${address}&token=${tokenid}&chain=8001`);
+  console.log("resp",resp);
+  var quoteDict = await resp.json();
+  quoteDict = quoteDict ? quoteDict :  {"quote":0.000001}
+  console.log('this is quoteDict', quoteDict)
+  var loanAmount = quoteDict.quote;
+  nftWorth.innerHTML = nftWorth.innerHTML + " " + loanAmount
   modal.style.display = "block";
   span.onclick = function() {
     nftWorth.innerHTML = nftWorth.innerHTML.replace(' a value not predicted yet', "")
     modal.style.display = "none";
   }
-
-  
+  var transferN
+  var transferM
   confirmBtn.onclick = async function() {
     modal.style.display = "none";
+    loadModal.style.display = "block";
     let user = await Moralis.User.current();
-    let currentPawned = user.get("pawnedNFTs") || [];
+    var currentPawned = user.get("pawnedNFTs") || [];
     let currentDate = new Date();
-    let data = { startDate: currentDate, address: `${address}`, interestRate: loanRate, loanAmount: loanAmount, loanDuration: loanDuration, image: imageURI, symbol: symbol, name: name, description: description, token: tokenid }
-    console.log("in on click");
+    tokenid = parseInt(tokenid) ? parseInt(tokenid) : tokenid;
+    var data = { startDate: currentDate, address: `${address}`, interestRate: loanRate, loanAmount: loanAmount, loanDuration: loanDuration, image: imageURI, symbol: symbol, name: name, description: description, token: tokenid }
+    console.log("in on click", data);
     try {
+      modal.style.display = "none";
+      await Moralis.enableWeb3();
+      transferN = await getNFTfromUser(address, tokenid);
+      transferM = await transferMaticToUser(loanAmount);
+      console.log('this is transferm and trasnfern', transferM, transferN);
+      console.log("this is currentpawned before push", currentPawned, data)
       currentPawned.push(data);
       user.set('pawnedNFTs', currentPawned);
-      console.log("user is ", user)
-      console.log("before curr pawned", currentPawned, data)
-    
-    await transferMaticToUser(loanAmount);
-    await getNFTfromUser(address, tokenid);
-
-    document.getElementById("content").innerHTML = ''
-      
-    await getNFTs();
-
-      
-    await user.save()
-    window.location.reload();
+      console.log('this is currentpawned after', currentPawned)
+      await user.save();
+      document.getElementById("content").innerHTML = ''
+      window.location.reload();
     } catch(err) {
-      console.log("There was an error", err)
+      console.log("There was an error", err, 'n', transferN, 'm', transferM)
     }
-
   }
   
 }
-
 
 
 async function getNFTfromUser(address, token) {
@@ -193,7 +199,7 @@ async function getNFTfromUser(address, token) {
   await Moralis.transfer(options);
   console.log('this is web3 provider', web3.provider)*/
   token = parseInt(token) ? parseInt(token) : token;
-let web3 = await Moralis.enableWeb3({});
+  let web3 = await Moralis.enableWeb3();
   console.log('in get nft')
   let userWallet = await getAddress();
   const options = {
@@ -203,25 +209,31 @@ let web3 = await Moralis.enableWeb3({});
     tokenId: token,
   };
   let transaction = await Moralis.transfer(options);
-  console.log('this is res', transaction)
+  let result = await transaction.wait();
+  return result;
 }
 
 
 async function giveNFTtoUser(address, token) {
+  const privateKey = process.env['privateKey']
   let userWallet = await getAddress();
   let web3 = await Moralis.enableWeb3({
-        chainId: 0x13881,
-        privateKey:
-          "3ec6ae487b65a47fe0633edca65a393ea05369b85f7cb58f6d24044172708f66"
+        chainId: 0x13881, 
+        privateKey: privateKey
   });
-  console.log('in get nft')
-  const options = {
-      type: "erc721",
+  token = parseInt(token) ? parseInt(token) : token;
+  console.log('inputs', address, token, userWallet, web3)
+  console.log('in get nft', token)
+  var options = {
+  type: "erc721",
   receiver: userWallet,
   contractAddress: address,
-    tokenId: token
-    };
-  await Moralis.transfer(options);
+  tokenId: token
+};
+  let transaction = await Moralis.transfer(options);
+  console.log('txn',transaction)
+  let result = await transaction.wait();
+  return result;
 }
 
 async function getMaticFromUser(value) {
@@ -235,6 +247,9 @@ async function getMaticFromUser(value) {
     receiver: "0x16b2f76CF7e35E4f0e516A9E8247A593FbddDDCD"
   };
   let transaction = await Moralis.transfer(options);
+  let result = await transaction.wait();
+  console.log("got matric",result);
+  return result;
 }
 
 
@@ -251,12 +266,13 @@ async function reclaimNFT(address) {
   var modal = document.getElementById("reclaimModal");
   var confirmBtn = document.getElementById("confirmReclaim");
   var span = document.getElementById("close-reclaim-modal");
+  var loadModal = document.getElementById("loadModal");
   var nftPrice = document.getElementById("NFT-price");
   let user = await Moralis.User.current();
   var currentPawned = user.get("pawnedNFTs") || [];
-  var NFT = currentPawned.filter(item => item.address == address)[0]
-  /* TODO ONCE PRICE PREDICTOR IS COMPLETE: ADD PRICE PREDICTOR
-  TO TEXT INSTEAD OF "NOT PREDICTED YET"*/
+  var NFT = null;
+  NFT = currentPawned.filter(item => item.address == address)[0]
+  console.log("this is nft", NFT, address)
   var now = new Date();
   var startDate = new Date(NFT.startDate);
   var dayDiff = Math.round((now.getTime() - startDate.getTime())/(1000 * 3600 * 24));
@@ -268,28 +284,31 @@ async function reclaimNFT(address) {
     nftPrice.innerHTML = nftPrice.innerHTML.replace(`${price}.`, "")
     modal.style.display = "none";
   }
-
+  var transferM
+  var transferN
   
   confirmBtn.onclick = async function() {
     try {
-      currentPawned = currentPawned.filter(item => item.address == address && item.token==NFT.token)
-    user.set('pawnedNFTs', currentPawned);
-      console.log("user is ", user)
+      modal.style.display = "none";
+      loadModal.style.display = "block";
       console.log("before curr reclaimed", currentPawned)
-    
-    await getMaticFromUser(NFT.loanAmount);
-    await giveNFTtoUser(address, NFT.token);
-
+    console.log('nft and addr', NFT, address)
+    transferM = await getMaticFromUser(NFT.loanAmount);
+    transferN = await giveNFTtoUser(address, NFT.token);
+      
+    let data = currentPawned.filter(item => item.address !== address && item.token!==NFT.token)
+    user.set('pawnedNFTs', data);
+      console.log("user is ", user)
 
     document.getElementById("content").innerHTML = ''
       
     await getNFTs();
 
       
-    await user.save()
+    await user.save();
     window.location.reload();
     } catch(err) {
-      console.log("There was an error", err)
+      console.log("There was an error", err, 'm', transferM, 'n', transferN)
     }
 
   }
--- static/js/moralisContract.js
+++ static/js/moralisContract.js
@@ -1,16 +1,354 @@
 'use strict'
 
 /* Moralis init code */
-const serverUrl = "https://gox0zhxekxir.usemoralis.com:2053/server";
-const appId = "1Wc4e9uFtXxdvJj3HvnN7irm6hrYAwei0znBZUka";
+//const serverUrl = "https://gox0zhxekxir.usemoralis.com:2053/server";
+//const appId = "1Wc4e9uFtXxdvJj3HvnN7irm6hrYAwei0znBZUka";
 Moralis.start({ serverUrl, appId });
 
 
 /* Contract Stuff */
-async function pawn(tokenid, address, amount, duration = 30, interestrate = 0.08) {
-let options = {
-  "contractAddress": "0xa513E6E4b8f2a923D98304ec87F64353C4D5C853",
-  "abi": [
+
+// pawn
+  
+async function executeContractScript(functionName, params) {
+
+
+let abi = [{
+      "inputs": [],
+      "stateMutability": "nonpayable",
+      "type": "constructor"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "borrower",
+          "type": "address"
+        },
+        {
+          "internalType": "uint256",
+          "name": "termIndex",
+          "type": "uint256"
+        }
+      ],
+      "name": "acceptTerms",
+      "outputs": [],
+      "stateMutability": "payable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "_amountInWei",
+          "type": "uint256"
+        },
+        {
+          "internalType": "uint256",
+          "name": "_interestRate",
+          "type": "uint256"
+        }
+      ],
+      "name": "calculatePayment",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "amount",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "pure",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "termIndex",
+          "type": "uint256"
+        }
+      ],
+      "name": "claimCollateral",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "lender",
+          "type": "address"
+        }
+      ],
+      "name": "getLoans",
+      "outputs": [
+        {
+          "components": [
+            {
+              "internalType": "contract IERC721",
+              "name": "nftAddress",
+              "type": "address"
+            },
+            {
+              "internalType": "uint256",
+              "name": "nftTokenID",
+              "type": "uint256"
+            },
+            {
+              "internalType": "uint256",
+              "name": "amountInWei",
+              "type": "uint256"
+            },
+            {
+              "internalType": "uint256",
+              "name": "interestRate",
+              "type": "uint256"
+            },
+            {
+              "internalType": "uint256",
+              "name": "startTime",
+              "type": "uint256"
+            },
+            {
+              "internalType": "uint256",
+              "name": "duration",
+              "type": "uint256"
+            },
+            {
+              "internalType": "address",
+              "name": "borrower",
+              "type": "address"
+            },
+            {
+              "internalType": "address",
+              "name": "lender",
+              "type": "address"
+            },
+            {
+              "internalType": "enum NFTPawnShop.CollateralStatus",
+              "name": "status",
+              "type": "uint8"
+            },
+            {
+              "internalType": "uint256",
+              "name": "borrowerIndex",
+              "type": "uint256"
+            },
+            {
+              "internalType": "uint256",
+              "name": "lenderIndex",
+              "type": "uint256"
+            },
+            {
+              "internalType": "uint256",
+              "name": "allTermsIndex",
+              "type": "uint256"
+            }
+          ],
+          "internalType": "struct NFTPawnShop.Terms[]",
+          "name": "",
+          "type": "tuple[]"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "borrower",
+          "type": "address"
+        }
+      ],
+      "name": "getPawnedTerms",
+      "outputs": [
+        {
+          "components": [
+            {
+              "internalType": "contract IERC721",
+              "name": "nftAddress",
+              "type": "address"
+            },
+            {
+              "internalType": "uint256",
+              "name": "nftTokenID",
+              "type": "uint256"
+            },
+            {
+              "internalType": "uint256",
+              "name": "amountInWei",
+              "type": "uint256"
+            },
+            {
+              "internalType": "uint256",
+              "name": "interestRate",
+              "type": "uint256"
+            },
+            {
+              "internalType": "uint256",
+              "name": "startTime",
+              "type": "uint256"
+            },
+            {
+              "internalType": "uint256",
+              "name": "duration",
+              "type": "uint256"
+            },
+            {
+              "internalType": "address",
+              "name": "borrower",
+              "type": "address"
+            },
+            {
+              "internalType": "address",
+              "name": "lender",
+              "type": "address"
+            },
+            {
+              "internalType": "enum NFTPawnShop.CollateralStatus",
+              "name": "status",
+              "type": "uint8"
+            },
+            {
+              "internalType": "uint256",
+              "name": "borrowerIndex",
+              "type": "uint256"
+            },
+            {
+              "internalType": "uint256",
+              "name": "lenderIndex",
+              "type": "uint256"
+            },
+            {
+              "internalType": "uint256",
+              "name": "allTermsIndex",
+              "type": "uint256"
+            }
+          ],
+          "internalType": "struct NFTPawnShop.Terms[]",
+          "name": "",
+          "type": "tuple[]"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "getWaitingTerms",
+      "outputs": [
+        {
+          "components": [
+            {
+              "internalType": "contract IERC721",
+              "name": "nftAddress",
+              "type": "address"
+            },
+            {
+              "internalType": "uint256",
+              "name": "nftTokenID",
+              "type": "uint256"
+            },
+            {
+              "internalType": "uint256",
+              "name": "amountInWei",
+              "type": "uint256"
+            },
+            {
+              "internalType": "uint256",
+              "name": "interestRate",
+              "type": "uint256"
+            },
+            {
+              "internalType": "uint256",
+              "name": "startTime",
+              "type": "uint256"
+            },
+            {
+              "internalType": "uint256",
+              "name": "duration",
+              "type": "uint256"
+            },
+            {
+              "internalType": "address",
+              "name": "borrower",
+              "type": "address"
+            },
+            {
+              "internalType": "address",
+              "name": "lender",
+              "type": "address"
+            },
+            {
+              "internalType": "enum NFTPawnShop.CollateralStatus",
+              "name": "status",
+              "type": "uint8"
+            },
+            {
+              "internalType": "uint256",
+              "name": "borrowerIndex",
+              "type": "uint256"
+            },
+            {
+              "internalType": "uint256",
+              "name": "lenderIndex",
+              "type": "uint256"
+            },
+            {
+              "internalType": "uint256",
+              "name": "allTermsIndex",
+              "type": "uint256"
+            }
+          ],
+          "internalType": "struct NFTPawnShop.Terms[]",
+          "name": "",
+          "type": "tuple[]"
+        },
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        },
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        },
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        },
+        {
+          "internalType": "bytes",
+          "name": "",
+          "type": "bytes"
+        }
+      ],
+      "name": "onERC721Received",
+      "outputs": [
+        {
+          "internalType": "bytes4",
+          "name": "",
+          "type": "bytes4"
+        }
+      ],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
     {
       "inputs": [
         {
@@ -43,16 +381,63 @@ let options = {
       "outputs": [],
       "stateMutability": "nonpayable",
       "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "_borrower",
+          "type": "address"
+        },
+        {
+          "internalType": "uint256",
+          "name": "termIndex",
+          "type": "uint256"
+        }
+      ],
+      "name": "paybackTerm",
+      "outputs": [],
+      "stateMutability": "payable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "delta",
+          "type": "uint256"
+        }
+      ],
+      "name": "refund",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "_borrower",
+          "type": "address"
+        },
+        {
+          "internalType": "uint256",
+          "name": "termIndex",
+          "type": "uint256"
+        }
+      ],
+      "name": "undoTerm",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
     }
-  ],
-  "Params": {
-    "_nftAddress": address,
-    "_nftTokenID":tokenid,
-    "_amountInWei": (amount*1000000000000000000),
-    "_duration": duration,
-    "_interestRate": interestrate
-  }
-}
-
-  
+  ]
+  const options = {
+    chain: "mumbai",
+    address: "0x5FbDB2315678afecb367f032d93F642f64180aa3",
+    function_name: functionName,
+    abi: abi,
+    params: params
+  };
+  const allowance = await Moralis.Web3API.native.runContractFunction(options);
 }
